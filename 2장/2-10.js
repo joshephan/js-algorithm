dae918e608edb1b0e052b23e24ebfb130ebe7104
Sanghun Han <joshephan0204@gmail.com>
2024-07-31 14:36:57 +0900

    fix

--- 2장/2-10.js
+++ 2장/2-10.js
@@ -1,9 +1,9 @@
 function selectionSort(array) {
   let indexMin, temp;
-  for (let i = 0; i < array.length - 1; i++) {
+  for (let i = 0; i < array.length - 1; i++) { 
     indexMin = i; // 변경의 대상이 되는 기준 원소의 인덱스
     for (let j = i + 1; j < array.length; j++) {
-      if (array[i] < array[indexMin]) {
+      if (array[j] < array[indexMin]) { // O(1)
         indexMin = j;
       }
     }
@@ -11,4 +11,5 @@ function selectionSort(array) {
     array[indexMin] = array[i];
     array[i] = temp;
   }
+  return array;
 }
--- 2장/2-17.js
+++ 2장/2-17.js
@@ -1,11 +1,11 @@
 function insertSort(array) {
-  for (let i = 1; i < array.legnth; i++) {
-    let currentValue = array[i];
-    let j;
-    for (j = i - 1; j >= 0 && array[j] > currentValue; j--) {
-      array[j + 1] = array[j];
+  for (let i = 1; i < array.length; i++) {
+    let currentValue = array[i]; 
+    let j; 
+    for (j = i - 1; j >= 0 && array[j] > currentValue; j--) { 
+      array[j + 1] = array[j]; 
     }
-    array[j + 1] = currentValue;
+    array[j + 1] = currentValue; 
   }
   return array;
 }
